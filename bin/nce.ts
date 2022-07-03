ec28e1dc2df31e7e8b36c5be7ff6fae1dada2651
Samuel MARLHENS <samuel.marlhens@gmail.com>
2022-07-03 16:05:28 +0200

    feat: use update-notifier to check for tool updates

--- bin/nce.ts
+++ bin/nce.ts
@@ -1,5 +1,20 @@
 #!/usr/bin/env node
+import updateNotifier from 'update-notifier';
+
 import { nce } from '../lib/index';
 import { cli } from '../lib/yargs';
+import packageJson from '../package.json';
+
+const notifier = updateNotifier({
+  pkg: packageJson,
+  updateCheckInterval: 1000 * 60,
+  shouldNotifyInNpmScript: true,
+});
+if (notifier.update && notifier.update.latest !== packageJson.version) {
+  notifier.notify({
+    defer: false,
+    isGlobal: true,
+  });
+}
 
 nce(cli);
