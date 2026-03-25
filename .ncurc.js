9973a447af0ea0ceb8dbd902c508cfc463fb9487
Samuel MARLHENS <samuel.marlhens@gmail.com>
2026-03-25 23:51:41 +0100

    chore: add knip v5

--- .ncurc.js
+++ .ncurc.js
@@ -3,23 +3,25 @@ module.exports = {
   target: name => {
     const targets = {
       // breaking changes to check
-      'typescript': 'minor',
+      typescript: 'minor',
 
       // following .nvmrc
       '@types/node': 'minor',
 
+      // pin to patch: v5.54+ pulls walk-up-path@4 which requires node 20
+      knip: 'patch',
       // require node 20
       '@trivago/prettier-plugin-sort-imports': 'minor',
       'find-up': 'minor',
       'lint-staged': 'minor',
-      'rimraf': 'minor',
+      rimraf: 'minor',
       'sort-package-json': 'minor',
-      'vitest': 'minor',
+      vitest: 'minor',
       '@vitest/': 'minor',
-      'yargs': 'minor',
+      yargs: 'minor',
 
       // require node 22
-      'listr2': 'minor',
+      listr2: 'minor',
     };
 
     const keys = Object.keys(targets);
