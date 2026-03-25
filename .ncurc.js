9b8e4f735a5355f47bc7cbb96d75d47ea640bef3
Samuel MARLHENS <samuel.marlhens@gmail.com>
2026-03-25 23:28:11 +0100

    chore: migrate ncurc to js and pin node-constrained deps to minor

---
+++ .ncurc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  reject: [],
+  target: name => {
+    const targets = {
+      // breaking changes to check
+      'typescript': 'minor',
+
+      // following .nvmrc
+      '@types/node': 'minor',
+
+      // require node 18.19
+      'execa': 'minor',
+
+      // require node 20
+      '@trivago/prettier-plugin-sort-imports': 'minor',
+      'find-up': 'minor',
+      'lint-staged': 'minor',
+      'rimraf': 'minor',
+      'sort-package-json': 'minor',
+      'vitest': 'minor',
+      '@vitest/': 'minor',
+      'yargs': 'minor',
+
+      // require node 22
+      'listr2': 'minor',
+    };
+
+    const keys = Object.keys(targets);
+    if (keys.some(key => new RegExp(key).test(name))) {
+      return targets[keys.find(key => new RegExp(key).test(name))];
+    }
+
+    return 'latest';
+  },
+};
