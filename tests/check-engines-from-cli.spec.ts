78da51f6ee502801636e00b8446de9b1e1172642
Samuel MARLHENS <samuel.marlhens@upciti.com>
2026-03-26 18:28:40 +0100

    refactor: replace execa with tinyexec

--- tests/check-engines-from-cli.spec.ts
+++ tests/check-engines-from-cli.spec.ts
@@ -1,44 +1,41 @@
-import { execa, type Options } from 'execa';
 import { resolve } from 'node:path';
+import { x } from 'tinyexec';
 import { describe, expect, it, vi } from 'vitest';
 
 describe('check engines from cli', () => {
   it('should check w/ package w/o engines w/o npmrc', async () => {
     vi.stubEnv('NODE_ENV', 'test');
 
-    const execaOptions: Options = {
-      cwd: resolve(process.cwd(), 'examples', 'without-engines-without-npmrc'),
-      stdio: 'pipe',
-      cleanup: true,
-      env: { NO_COLOR: '1' },
-    };
-    const { stdout } = await execa('node', [resolve(process.cwd(), 'dist', 'bin', 'nce.js')], execaOptions);
-    expect(stdout).toMatchSnapshot();
+    const result = await x('node', [resolve(process.cwd(), 'dist', 'bin', 'nce.js')], {
+      nodeOptions: {
+        cwd: resolve(process.cwd(), 'examples', 'without-engines-without-npmrc'),
+        env: { ...process.env, NO_COLOR: '1' },
+      },
+    });
+    expect(result.stdout).toMatchSnapshot();
   }, 10000);
 
   it('should check w/ package w/o engines w/ npmrc w/o engine-strict', async () => {
     vi.stubEnv('NODE_ENV', 'test');
 
-    const execaOptions: Options = {
-      cwd: resolve(process.cwd(), 'examples', 'without-engines-with-npmrc-without-engine-strict'),
-      stdio: 'pipe',
-      cleanup: true,
-      env: { NO_COLOR: '1' },
-    };
-    const { stdout } = await execa('node', [resolve(process.cwd(), 'dist', 'bin', 'nce.js')], execaOptions);
-    expect(stdout).toMatchSnapshot();
+    const result = await x('node', [resolve(process.cwd(), 'dist', 'bin', 'nce.js')], {
+      nodeOptions: {
+        cwd: resolve(process.cwd(), 'examples', 'without-engines-with-npmrc-without-engine-strict'),
+        env: { ...process.env, NO_COLOR: '1' },
+      },
+    });
+    expect(result.stdout).toMatchSnapshot();
   }, 10000);
 
   it('should check w/ package w/o engines w/ npmrc w/ engine-strict', async () => {
     vi.stubEnv('NODE_ENV', 'test');
 
-    const execaOptions: Options = {
-      cwd: resolve(process.cwd(), 'examples', 'without-engines-with-npmrc-with-engine-strict'),
-      stdio: 'pipe',
-      cleanup: true,
-      env: { NO_COLOR: '1' },
-    };
-    const { stdout } = await execa('node', [resolve(process.cwd(), 'dist', 'bin', 'nce.js')], execaOptions);
-    expect(stdout).toMatchSnapshot();
+    const result = await x('node', [resolve(process.cwd(), 'dist', 'bin', 'nce.js')], {
+      nodeOptions: {
+        cwd: resolve(process.cwd(), 'examples', 'without-engines-with-npmrc-with-engine-strict'),
+        env: { ...process.env, NO_COLOR: '1' },
+      },
+    });
+    expect(result.stdout).toMatchSnapshot();
   }, 10000);
 });
